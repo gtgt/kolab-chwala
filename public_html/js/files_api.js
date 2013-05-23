5b83137d8314ef0d41942bb88ce3c8edf3136a30
Aleksander Machniak <alec@alec.pl>
2013-05-23 15:46:04 +0200

    Remove PDF detection code which invokes warnings in browsers in some configurations

--- public_html/js/files_api.js
+++ public_html/js/files_api.js
@@ -470,14 +470,6 @@ function files_api()
         return 1;
     }
 
-    var obj = document.createElement('OBJECT');
-    obj.onload = function() { ref.env.browser_capabilities.pdf = 1; };
-    obj.onerror = function() { ref.env.browser_capabilities.pdf = 0; };
-    obj.style.display = 'none';
-    obj.type = 'application/pdf';
-    obj.data = this.env.resources_dir + '/blank.pdf';
-    document.body.appendChild(obj);
-
     return 0;
   };
 
